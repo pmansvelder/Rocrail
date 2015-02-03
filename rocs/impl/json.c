89efc4ee06df46d2d2fbf1e43517d0779698c72b
Rob Versluis <r.j.versluis@rocrail.net>
2015-02-03 16:44:50 +0100

    json: parser

--- rocs/impl/json.c
+++ rocs/impl/json.c
@@ -74,15 +74,57 @@ static void* __event( void* inst, const void* evt ) {
 /** ----- OJSon ----- */
 
 
-/** Convert XML to a JSon string. */
-static char* _toJSon( iONode xml ) {
-  return NULL;
+/**  */
+static char* __toJSonString( iONode xml, int level ) {
+  int      i = 0;
+  char*  str = NULL;
+  char*  fmt = NULL;
+
+  if( level == 0 )
+    fmt = StrOp.fmt( "{" );
+  else
+    fmt = StrOp.fmt( "\"%s\": {", NodeOp.getName(xml) );
+  str = StrOp.cat( str, fmt );
+  StrOp.free( fmt );
+  for( i = 0; i < NodeOp.getAttrCnt(xml); i++ ) {
+    iOAttr a = NodeOp.getAttr(xml,i);
+    long len;
+    fmt = StrOp.fmt( "\"%s\": \"%s\"", AttrOp.getName(a), AttrOp.getVal(a) );
+    str = StrOp.cat( str, fmt );
+    if( i + 1 < NodeOp.getAttrCnt(xml) || NodeOp.getChildCnt(xml) > 0)
+      str = StrOp.cat( str, "," );
+    str = StrOp.cat( str, " " );
+    StrOp.free( fmt );
+  }
+
+  if( NodeOp.getChildCnt(xml) == 0 ) {
+    str = StrOp.cat( str, "} " );
+    return str;
+  }
+
+  TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "child count = %d", NodeOp.getChildCnt(xml));
+  for( i = 0; i < NodeOp.getChildCnt(xml); i++ ) {
+    iONode child = NodeOp.getChild(xml,i);
+    fmt = __toJSonString( child, level + 1 );
+    str = StrOp.cat( str, fmt );
+    StrOp.free( fmt );
+  }
+
+  fmt = StrOp.fmt( "} " );
+  str = StrOp.cat( str, fmt );
+  StrOp.free( fmt );
+
+  return str;
 }
 
 
+static char* _toJSon( iONode xml ) {
+  return __toJSonString(xml, 0);
+}
+
 /*
 {
-    "name": "Philips hue",
+    "name": "xSpooler",
     "zigbeechannel": 15,
     "mac": "00:17:88:00:00:00",
     "dhcp": true,
@@ -93,24 +135,6 @@ static char* _toJSon( iONode xml ) {
     "proxyport": 0,
     "UTC": "2014-07-17T09:27:35",
     "localtime": "2014-07-17T11:27:35",
-    "timezone": "Europe/Madrid",
-    "whitelist": {
-        "ffffffffe0341b1b376a2389376a2389": {
-            "last use date": "2014-07-17T07:21:38",
-            "create date": "2014-04-08T08:55:10",
-            "name": "PhilipsHueAndroidApp#TCT ALCATEL ONE TOU"
-        },
-        "pAtwdCV8NZId25Gk": {
-            "last use date": "2014-05-07T18:28:29",
-            "create date": "2014-04-09T17:29:16",
-            "name": "MyApplication"
-        },
-        "gDN3IaPYSYNPWa2H": {
-            "last use date": "2014-05-07T09:15:21",
-            "create date": "2014-05-07T09:14:38",
-            "name": "iPhone Web 1"
-        }
-    },
     "swversion": "01012917",
     "apiversion": "1.3.0",
     "swupdate": {
@@ -118,105 +142,233 @@ static char* _toJSon( iONode xml ) {
         "url": "",
         "text": "",
         "notify": false
-    },
-    "linkbutton": false,
-    "portalservices": false,
-    "portalconnection": "connected",
-    "portalstate": {
-        "signedon": true,
-        "incoming": false,
-        "outgoing": true,
-        "communication": "disconnected"
     }
 }
-*/
 
-static iONode __parseAttribute( const char* s, iONode xml, int* mainIdx) {
-  iONode child = NULL;
-  int i = 0;
-  char* attName = StrOp.dup(s);
-  char* attVal  = StrOp.dup(s);
+
+
+[
+  {"success":{"/lights/1/state/bri":200}},
+  {"success":{"/lights/1/state/on":true}},
+  {"success":{"/lights/1/state/hue":50000}}
+]
+
+{"deviceid":["45AF34","543636","34AFBE"]}
+
+{
+  "7": {"name": "Hue Lamp 7"},
+  "8": {"name": "Hue Lamp 8"},
+  "lastscan": "2012-10-29T12:00:00"
+}
+
+
+*/
+static iONode __parseNode(const char* nodeName, const char* json, int* idx) {
+  iONode xml = NULL;
+  int len = StrOp.len(json);
+  Boolean startName = False;
+  Boolean endName   = False;
+  Boolean pendingVal = False;
+  Boolean startVal = False;
+  Boolean endVal   = False;
+  Boolean strVal   = False;
+  Boolean startArray = False;
+  char* attName = StrOp.dup(json);
+  char* attVal  = StrOp.dup(json);
   int attNameIdx = 0;
   int attValIdx  = 0;
-
-  iOStrTok tok = StrTokOp.inst( s, ':');
-
   attName[0] = '\0';
   attVal [0] = '\0';
 
-  if( StrTokOp.hasMoreTokens(tok) ) {
-    const char* aname = StrTokOp.nextToken(tok);
-    int len = StrOp.len(aname);
-    Boolean startName = False;
-    for( i = 0; i < len; i++ ) {
-      if( aname[i] == '\"' && !startName ) {
-        startName = True;
+  if( nodeName == NULL ) {
+
+  }
+  else {
+    xml = NodeOp.inst( nodeName, NULL, ELEMENT_NODE );
+    TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "new node <%s>", nodeName );
+  }
+
+  while( *idx < len ) {
+    if( json[*idx] == '}' ) {
+      if( pendingVal && nodeName != NULL ) {
+        if( StrOp.len(attName) > 0 ) {
+          TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "new attribute %s=\"%s\"", attName, attVal );
+          NodeOp.setStr( xml, attName, attVal );
+        }
+        else {
+          TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "value without name \"%s\"", attVal );
+        }
       }
-      else if( aname[i] != '\"' && startName ) {
-        attName[attNameIdx] = aname[i];
+      TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "end node <%s>", nodeName );
+      return xml;
+    }
+    else if( json[*idx] == '\"' && !startName ) {
+      startName = True;
+    }
+    else if( startName && !endName ) {
+      if( json[*idx] == '\"' ) {
+        endName = True;
+        TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "new attribute name \"%s\"", attName );
+      }
+      else {
+        attName[attNameIdx] = json[*idx];
         attNameIdx++;
         attName[attNameIdx] = '\0';
       }
     }
-    TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "attribute name [%s]", attName);
-  }
-
-  if( StrTokOp.hasMoreTokens(tok) ) {
-    const char* aval = StrTokOp.nextToken(tok);
-    int len = StrOp.len(aval);
-    Boolean startVal = False;
-    for( i = 0; i < len; i++ ) {
-      if( aval[i] != ' ' && !startVal ) {
-        if( aval[i] == '{' ) {
-          TraceOp.trc( name, TRCLEVEL_INFO, __LINE__, 9999, "new child node <%s>", attName);
-          child = NodeOp.inst(attName, xml, ELEMENT_NODE);
-          iONode childchild = __parseAttribute(aval, child, mainIdx);
-          if( childchild != NULL ) {
-            NodeOp.addChild( child, childchild );
+    else if( endName ) {
+      if( json[*idx] == ':' ) {
+        pendingVal = True;
+        TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "pending value..." );
+      }
+      else if( pendingVal ) {
+        if( json[*idx] == '{' ) {
+          iONode child = __parseNode(attName, json, idx);
+          if( child != NULL ) {
+            if( xml == NULL )
+              xml = child;
+            else
+              NodeOp.addChild(xml, child);
           }
-          break;
+          startName = False;
+          endName   = False;
+          pendingVal = False;
+          startVal = False;
+          endVal   = False;
+          strVal   = False;
+          startArray = False;
+          attNameIdx = 0;
+          attValIdx  = 0;
+          attName[0] = '\0';
+          attVal [0] = '\0';
+        }
+        else if( json[*idx] == '[' && !startVal && !endVal) {
+          startArray = True;
+          TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "start array..." );
+        }
+        else if( json[*idx] == ']' && startArray) {
+          startArray = False;
+          TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "end array..." );
+        }
+        else if( json[*idx] == '\"' && !startVal && !endVal) {
+          startVal = True;
+          strVal   = True;
+        }
+        else if( json[*idx] > ' ' && !startVal && !endVal) {
+          startVal = True;
+          attVal[attValIdx] = json[*idx];
+          attValIdx++;
+          attVal[attValIdx] = '\0';
+          TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "value \"%s\"...", attVal );
+        }
+        else if( startVal && !endVal ) {
+          if( json[*idx] == '\"' || (!strVal && json[*idx] == '}' ) || (!strVal && json[*idx] == ',' ) ) {
+            endVal = True;
+
+            if( startArray ) {
+              startVal = False;
+              endVal   = False;
+              TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "array value \"%s\"...", attVal );
+            }
+            else {
+              if( StrOp.len(attName) > 0 ) {
+                TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "new attribute %s=\"%s\"", attName, attVal );
+                NodeOp.setStr( xml, attName, attVal );
+              }
+              else {
+                TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "value without name \"%s\"", attVal );
+              }
+              startName = False;
+              endName   = False;
+              pendingVal = False;
+              startVal = False;
+              endVal   = False;
+              strVal   = False;
+              startArray = False;
+              attNameIdx = 0;
+              attValIdx  = 0;
+              attName[0] = '\0';
+              attVal [0] = '\0';
+            }
+          }
+          else {
+            attVal[attValIdx] = json[*idx];
+            attValIdx++;
+            attVal[attValIdx] = '\0';
+            //TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "def value \"%s\"...", attVal );
+
+            if( !strVal && (StrOp.equals("true", attVal) || StrOp.equals("false", attVal) || StrOp.equals("null", attVal) ) ) {
+              endVal = True;
+              if( StrOp.len(attName) > 0 ) {
+                TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "new attribute %s=\"%s\"", attName, attVal );
+                NodeOp.setStr( xml, attName, attVal );
+              }
+              startName = False;
+              endName   = False;
+              pendingVal = False;
+              startVal = False;
+              endVal   = False;
+              strVal   = False;
+              startArray = False;
+              attNameIdx = 0;
+              attValIdx  = 0;
+              attName[0] = '\0';
+              attVal [0] = '\0';
+            }
+          }
         }
         else {
-          startVal = True;
+          TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "drop -\"%c\"-...", json[*idx] );
         }
-      }
-
-      if( aval[i] != '\"' && aval[i] != '}' && startVal ) {
-        attVal[attValIdx] = aval[i];
-        attValIdx++;
-        attVal[attValIdx] = '\0';
       }
     }
-  }
-
-  if( child == NULL && StrOp.len(attName) > 0 ) {
-    NodeOp.setStr( xml, attName, attVal );
-  }
-
-  return child;
-}
-
-static iONode _toXML( const char* json ) {
-  iONode xml = NodeOp.inst( "json", NULL, ELEMENT_NODE );
-  int len = StrOp.len(json);
-  int i = 0;
-
-  for( i = 0; i < len; i++ ) {
-    if( json[i] == '{' ) {
-      /* start op node contents */
-      iOStrTok tok = StrTokOp.inst( json+i+1, ',');
-      while( StrTokOp.hasMoreTokens(tok) ) {
-        const char* s = StrTokOp.nextToken(tok);
-        iONode child = __parseAttribute(s, xml, &i);
-        if( child != NULL ) {
-          NodeOp.addChild( xml, child );
-        }
-      }
+    *idx = *idx + 1;
+    if( *idx >= len ) {
+      TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "idx == len (%d, %d) ", *idx, len );
     }
   }
 
   return xml;
 }
+
+
+static iONode _toXML( const char* json ) {
+  iONode xml = NULL;
+  int len = StrOp.len(json);
+  int idx = 0;
+  int i = 0;
+
+  for( i = 0; i < len; i++ ) {
+
+    if( json[i] == '[' ) {
+      xml = NodeOp.inst( "json", NULL, ELEMENT_NODE );
+      iONode child = NULL;
+      idx = i + 1;
+      do {
+        TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "***** %.20s", json+idx);
+        child = __parseNode(NULL, json, &idx);
+        if( child != NULL ) {
+          NodeOp.addChild(xml, child);
+          const char* p = StrOp.findc(json+idx, '{');
+          if( p != NULL ) {
+            idx = p - json;
+          }
+        }
+      } while( child != NULL );
+      break;
+    }
+
+    if( json[i] == '{' ) {
+      /* root node */
+      idx = i + 1;
+      xml = __parseNode("json", json, &idx);
+      break;
+    }
+  }
+
+  return xml;
+}
+
 
 
 /* ----- DO NOT REMOVE OR EDIT THIS INCLUDE LINE! -----*/
