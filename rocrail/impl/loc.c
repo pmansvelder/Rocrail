1074f4d62407a58e47b669b8e7ef855cf1c8e2f8
Rob Versluis <r.j.versluis@rocrail.net>
2015-03-10 18:17:26 +0100

    loc: deceleration fix

--- rocrail/impl/loc.c
+++ rocrail/impl/loc.c
@@ -1406,17 +1406,19 @@ static Boolean __engine( iOLoc inst, iONode cmd ) {
       if( data->curSpeed == 0 && data->drvSpeed )
         data->curSpeed = data->drvSpeed;
 
-      TraceOp.trc( name, TRCLEVEL_DEBUG, __LINE__, 9999, "*****Vcur=%d Vdrv=%d mode=[%s]", data->curSpeed, data->drvSpeed, wLoc.getmode(data->props) );
-      if( data->curSpeed > data->drvSpeed  > 0 && data->curSpeed > wLoc.getV_min(data->props) ) {
+      if( data->curSpeed > data->drvSpeed && data->curSpeed > wLoc.getV_min(data->props) ) {
         int dif = data->curSpeed - data->drvSpeed;
         int Vdif = (dif * wLoc.getdecelerate(data->props)) / 100;
         if( Vdif < 1 )
           Vdif = 1;
         data->curSpeed -= Vdif;
-        TraceOp.trc( name, TRCLEVEL_USER1, __LINE__, 9999, "V=%d Vdif=%d", data->curSpeed, Vdif );
+        TraceOp.trc( name, TRCLEVEL_USER1, __LINE__, 9999, "Vcur=%d Vdrv=%d Vdif=%d", data->curSpeed, data->drvSpeed, Vdif );
         if( cmd == NULL )
           cmd = NodeOp.inst( wLoc.name(), NULL, ELEMENT_NODE );
         wLoc.setV( cmd, data->curSpeed );
+      }
+      else {
+        data->curSpeed = data->drvSpeed;
       }
     }
   }
