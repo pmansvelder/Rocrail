1ae6bd20320bd55172e8040325e19da9ffbe582c
Rob Versluis <r.j.versluis@rocrail.net>
2015-04-17 10:52:44 +0200

    rocweb: prototyping (very time consuming; save it in git)

---
+++ rocrail/impl/web/html/playground/rocweb.js
@@ -0,0 +1,18 @@
+
+function openThrottle()
+{
+  $( "#popupThrottle" ).popup( "open" );
+}
+
+function closeThrottle()
+{
+  $( "#popupThrottle" ).popup( "close" );
+}
+
+function openOptions()
+{
+}
+
+function closeOptions()
+{
+}
