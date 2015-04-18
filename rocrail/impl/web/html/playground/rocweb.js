9d87b4ba6287d6466807d60a5ed1b3bd8857eaab
Rob Versluis <r.j.versluis@rocrail.net>
2015-04-18 08:13:46 +0200

    rocweb: prototyping

--- rocrail/impl/web/html/playground/rocweb.js
+++ rocrail/impl/web/html/playground/rocweb.js
@@ -1,3 +1,4 @@
+var req;
 
 function openInfo()
 {
@@ -45,6 +46,25 @@ function closeOptions()
 {
 }
 
+function actionSensor(id)
+{
+  console.log("sensor action on " + id );
+  //alert("flip sensor " + id);
+ 
+  // send an XMLHttpRequest
+  try {
+    req = new XMLHttpRequest();
+    req.onreadystatechange = processReqChange;
+    req.open("GET", "rocweb.xml?fb_flip="+id.replace("fb_",""), true);
+    req.send("");
+  } 
+    catch(e) {
+      alert("exception: " + e);
+    }
+  }
+
+
+
 function actionBlock(id)
 {
   localStorage.setItem("block", id);
@@ -69,4 +89,128 @@ $(document).on("pagecreate",function(){
   } );
 
 });
+
+
+function processReqChange() {
+//only if req shows "loaded"
+if (req.readyState == 4) {
+ //alert("response status = "+req.status);
+ if (req.status == 0) {
+   try {
+     xmlDoc = req.responseXML;
+     fblist = xmlDoc.getElementsByTagName("fb");
+     for (var i = 0; i < fblist.length; i++) {
+       var div = document.getElementById("fb_"+fblist[i].getAttribute('id'));
+       if( "true" == fblist[i].getAttribute('state') )
+         div.style.backgroundImage = "url(sensor_on_1.png)";
+       else
+         div.style.backgroundImage = "url(sensor_off_1.png)";
+     }
+   }
+   catch(e) {
+     alert("exception: " + e);
+   }
+
+ }
+ if (req.status == 200) {
+   // ...processing statements go here...
+   alert("response received");
+   }
+ }
+}
+
+function loadPlan() {
+ // send an XMLHttpRequest
+ try {
+   req = new XMLHttpRequest();
+   req.onreadystatechange = processReqPlan;
+   req.open("GET", "plan.xml", true);
+   req.send("");
+ } 
+   catch(e) {
+     alert("exception: " + e);
+   }
+}
+
+function xml2string(node) {
+  if (typeof(XMLSerializer) !== 'undefined') {
+     var serializer = new XMLSerializer();
+     return serializer.serializeToString(node);
+  } else if (node.xml) {
+     return node.xml;
+  }
+}
+
+function processReqPlan() {
+//only if req shows "loaded"
+if (req.readyState == 4) {
+ // only if "OK"
+ //alert("response status = "+req.status);
+ if (req.status == 0) {
+   try {
+     xmlDoc = req.responseXML;
+     
+     fblist = xmlDoc.getElementsByTagName("fb");
+     for (var i = 0; i < fblist.length; i++) {
+       console.log('sensor: ' + fblist[i].getAttribute('id'));
+       var newdiv = document.createElement('div');
+       newdiv.setAttribute('id', "fb_"+fblist[i].getAttribute('id'));
+       newdiv.setAttribute('onClick', "actionSensor(this.id)");
+       newdiv.setAttribute('class', "item");
+       newdiv.style.position = "absolute";
+       newdiv.style.width    = "32px";
+       newdiv.style.height   = "32px";
+       newdiv.style.left     = "" + (parseInt(fblist[i].getAttribute('x')) * 32) + "px";
+       newdiv.style.top      = "" + (parseInt(fblist[i].getAttribute('y')) * 32) + "px";
+       newdiv.innerHTML      = "";
+       
+       if( "true" == fblist[i].getAttribute('state') )
+         newdiv.style.backgroundImage = "url('img/sensor_on_1.png')";
+       else
+         newdiv.style.backgroundImage = "url('img/sensor_off_1.png')";
+
+       document.body.appendChild(newdiv);
+     }
+     
+     bklist = xmlDoc.getElementsByTagName("bk");
+     for (var i = 0; i < bklist.length; i++) {
+       console.log('block: ' + bklist[i].getAttribute('id'));
+       var newdiv = document.createElement('div');
+       newdiv.setAttribute('id', "bk_"+bklist[i].getAttribute('id'));
+       newdiv.setAttribute('onClick', "actionBlock(this.id)");
+       newdiv.setAttribute('class', "item");
+       newdiv.style.position = "absolute";
+       newdiv.style.width    = "128px";
+       newdiv.style.height   = "32px";
+       newdiv.style.lineHeight = "32px";
+       newdiv.style.left     = "" + (parseInt(bklist[i].getAttribute('x')) * 32) + "px";
+       newdiv.style.top      = "" + (parseInt(bklist[i].getAttribute('y')) * 32) + "px";
+       newdiv.innerHTML      = "<label class='itemtext'>"+bklist[i].getAttribute('locid')+"</label>";
+       newdiv.style.textAlign= "center";
+       
+       console.log(xml2string(bklist[i]));
+       console.log("block entering=" + bklist[i].getAttribute("entering") + " x,y " + newdiv.style.left + "," + newdiv.style.top );
+
+       if( "true" == bklist[i].getAttribute('entering') )
+         newdiv.style.backgroundImage = "url('img/block_enter_1.png')";
+       else if( "closed" == bklist[i].getAttribute('state') )
+         newdiv.style.backgroundImage = "url('img/block_closed_1.png')";
+       else
+         newdiv.style.backgroundImage = "url('img/block_occ_1.png')";
+
+       document.body.appendChild(newdiv);
+     }
+   }
+   catch(e) {
+     alert("exception: " + e);
+   }
+
+ }
+ if (req.status == 200) {
+   // ...processing statements go here...
+   alert("response received");
+   }
+ }
+}
+
 
