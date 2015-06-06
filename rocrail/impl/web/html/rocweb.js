57fc649a1821076fc094a4ddfa78de6ace52dc7d
Rob Versluis <r.j.versluis@rocrail.net>
2015-06-06 13:05:21 +0200

    rocweb: line height for text objects

--- rocrail/impl/web/html/rocweb.js
+++ rocrail/impl/web/html/rocweb.js
@@ -2337,13 +2337,13 @@ function getTextImage(txNode, div) {
       pointsize = "10";
     if( text == undefined )
       text = "";
-    
+    var height = div.style.height;
     if( ori == "north" )
-      div.innerHTML = "<div class='itemtextV' style='font-size:"+pointsize+"px;'>"+getTextFormat(txNode)+"</div>";
+      div.innerHTML = "<div class='itemtextV' style='line-height:"+height+"; font-size:"+pointsize+"px;'>"+getTextFormat(txNode)+"</div>";
     else if( ori == "south" )
-      div.innerHTML = "<div class='itemtextV' style='font-size:"+pointsize+"px;'>"+getTextFormat(txNode)+"</div>";
+      div.innerHTML = "<div class='itemtextV' style='line-height:"+height+";font-size:"+pointsize+"px;'>"+getTextFormat(txNode)+"</div>";
     else
-      div.innerHTML = "<div class='itemtext' style='font-size:"+pointsize+"px;'>" +getTextFormat(txNode)+ "</div>";
+      div.innerHTML = "<div class='itemtext' style='line-height:"+height+";font-size:"+pointsize+"px;'>" +getTextFormat(txNode)+ "</div>";
   }
   
 }
