017aefd7f015c84e77318cbe7046c7adc692ad49
Rob Versluis <r.j.versluis@rocrail.net>
2015-06-12 16:15:00 +0200

    rocweb: fix for loco speed update

--- rocrail/impl/web/html/rocweb.js
+++ rocrail/impl/web/html/rocweb.js
@@ -1,3 +1,22 @@
+/*
+ Rocrail - Model Railroad Software
+
+ Copyright (C) 2002-2015 Rob Versluis, Rocrail.net
+
+ This program is free software; you can redistribute it and/or
+ modify it under the terms of the GNU General Public License
+ as published by the Free Software Foundation; either version 2
+ of the License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with this program; if not, write to the Free Software
+ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
 /* Variables */
 var req;
 var yoffset = 48;
@@ -2663,9 +2682,11 @@ function handleLoco(lc) {
     }
   }
 
-  var V = parseInt(lc.getAttribute('V'));
-  speedUpdateVal = V;
-
+  if( lc.getAttribute('id') == locoSelected && lc.getAttribute('V') != undefined ) {
+    var V = parseInt(lc.getAttribute('V'));
+    speedUpdateVal = V;
+  }
+  
   if( ! $(popupThrottle).parent().hasClass('ui-popup-hidden') ) {
     if( lc.getAttribute('id') == locoSelected ) {
       initThrottleStatus();
