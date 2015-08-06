1b6ab62a9e297dffe6165901cf77b322e700d976
Rob Versluis <r.j.versluis@rocrail.net>
2015-08-06 13:03:16 +0200

    cleanup

--- rocdigs/impl/ddx/init.c
+++ rocdigs/impl/ddx/init.c
@@ -1,23 +1,7 @@
 /*
  Rocrail - Model Railroad Software
- 
  Copyright (c) 2002-2015 Robert Jan Versluis, Rocrail.net
-
- Some parts are copied from the DDL project of Torsten Vogt: http://www.vogt-it.com/OpenSource/DDL
- 
- This program is free software; you can redistribute it and/or
- modify it under the terms of the GNU General Public License
- as published by the Free Software Foundation; either version 2
- of the License, or (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with this program; if not, write to the Free Software
- Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ All rights reserved.
 */
 #include "rocdigs/impl/ddx_impl.h"
 
